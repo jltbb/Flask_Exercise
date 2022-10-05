da72d22fa48b5d0e8c6d5dd1a1880f1038986e83
jltbb <81332545+jltbb@users.noreply.github.com>
2022-10-05 16:56:46 -0400

    Flasking flask flask (Finish part 2)

--- app.py
+++ app.py
@@ -12,17 +12,18 @@ global studentOrganisationDetails
 def index():
     # Complete this function to get current date and time assign this value to currentDate, display this data on index.html
 
-    return render_template('index.html', currentDate=datetime.datetime.now())
+    return render_template('index.html', currentDate=datetime.datetime.now().replace(microsecond=0))
 
 
 @app.get('/calculate')
 def displayNumberPage():
     # Complete this function to display form.html page
-    pass
+    return render_template('form.html')
 
-
-@app.route('/calculate', methods=['POST'])
+import math
+@app.route('/result', methods=['POST'])
 def checkNumber():
+    oddOrEven=''
     # Get Number from form and display message according to number
     # Display "Number {Number} is even" if given number is even on result.html page
     # Display "Number {Number} is odd" if given number is odd on result.html page
@@ -32,6 +33,19 @@ def checkNumber():
     number = request.form['number']
 
     # Write your to code here to check whether number is even or odd and render result.html page
+    if number == '':
+        oddOrEven='No number provided'
+    elif not number.isnumeric():
+        oddOrEven='Provided input is not an integer!'
+    elif len(number) > 4300:
+        oddOrEven='Your number is too large! (Flask breaks above this amount!)'
+    elif int(number) % 2 == 0:
+        oddOrEven=number + ' is even!'
+    else: 
+        oddOrEven=number + ' is odd!'
+
+    return render_template('result.html', oddOrEven=oddOrEven)
+
 
 
 @app.get('/addStudentOrganisation')
